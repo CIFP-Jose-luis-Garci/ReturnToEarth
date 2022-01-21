3e367ac64fea88ce5d98369cb094de1866666d15
Sergio <Sergiojurado111@gmail.com>
2022-01-21 20:33:02 +0100

    Animación de las puertas con script
    Se abren las puertas cuando pasa un muñeco

---
+++ Unity/ReturnToEarth_v01/Assets/02_Scripts/Puertas.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puertas : MonoBehaviour
+{
+    public GameObject[] puertas;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+       puertas = GameObject.FindGameObjectsWithTag("puertas");
+       animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Personajes")
+        {
+            animator.SetBool("Open", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Personajes")
+        { 
+            animator.SetBool("Open", false);
+        }
+    }
+}
